1b689abda2bcd20d83361059ec61300b12b1f92c
Mohsen Ghorbani <m.ghorbani2357@gmail.com>
2021-02-24 22:38:13 +0330

    Initial commit

---
+++ CONTRIBUTIONS.rst
@@ -0,0 +1,5 @@
+Contributions
+=============
+
+* `Mohsen Ghorbani <https://github.com/mghorbani2357>`_
+* `Mohammad Mehdi Mohammadi <https://github.com/linzstadler>`_
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""setup.py for benchtest"""
+
+__author__ = "Mohsen Ghorbani"
+__email__ = "m.ghorbani2357@gmail.com"
+__copyright__ = "Copyright 2021, Mohsen Ghorbani"
+
+from setuptools import setup, find_packages
+import os
+import versioneer
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+
+REQUIREMENTS = filter(None, open('requirements.txt').read().splitlines())
+
+setup(
+    name='benchtest',
+    author=__author__,
+    author_email=__email__,
+    url="https://github.com/mghorbani2357/benchtest",
+    license="MIT",
+    zip_safe=False,
+    packages=find_packages(exclude=["tests*"]),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    classifiers=[k for k in open('CLASSIFIERS').read().split('\n') if k],
+    install_requires=list(REQUIREMENTS),
+    long_description=open('README.rst').read() + open('HISTORY.rst').read(),
+    long_description_content_type='text/x-rst',
+    description='',
+)
