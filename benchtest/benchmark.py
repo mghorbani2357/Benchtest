a427eb5c7d5ddc953162f1f4c83261a2f5962cbf
Mohsen Ghorbani <m.ghorbani2357@gmail.com>
2021-02-27 23:14:08 +0330

    Initial commit

--- benchtest/benchmark.py
+++ benchtest/benchmark.py
@@ -1,3 +1,24 @@
-class TestCase:
+from functools import wraps
+import time
+import json
+
+
+class BenchMark:
+    report = dict()
+
     def __init__(self):
         pass
+
+    def generate_report(self):
+        print(json.dumps(self.report))
+
+    def bench_test(self, name, iterations=100, input_generator=None):
+        def decorator(function):
+            beginning_time = time.time()
+            for i in range(iterations):
+                function()
+            # if name == '':
+            name = function.__name__
+            self.report[name] = (time.time() - beginning_time) / iterations
+
+        return decorator
---
+++ tests/testbench.py
@@ -0,0 +1,52 @@
+# # from benchtest.benchmark import BenchMark
+#
+# benchmark = BenchMark()
+#
+#
+# def data_generator():
+#     return [None] * 10 ** 6
+#
+#
+# lst = data_generator()
+#
+#
+# @benchmark.bench_test('test')
+# def bench_test_list():
+#     k = 'a' in lst
+#
+#
+# benchmark.generate_report()
+
+import cProfile
+import pstats
+import profile
+
+# pr = profile.Profile(your_time_func)
+lst = [None] * 10 ** 6
+
+
+def search_list():
+    k = 'a' in lst
+
+
+def bench_test_list():
+    for i in range(100):
+        search_list()
+
+
+pr = cProfile.Profile()
+pr.enable()
+
+
+bench_test_list()
+
+pr.disable()
+
+ps = pstats.Stats(pr).sort_stats('tottime')
+ps.print_stats()
+
+
+# import profile
+# pr = profile.Profile()
+# for i in range(5):
+#     print(pr.calibrate(10000))
